52bef431a717dfc38af75522768cb42d87b48b00
aragami1566 <phu-hung.dang@epita.fr>
2023-07-03 21:19:48 +0200

    added kill monkey + vfx paradis tout droit

--- Game/Assets/Scripts/Interaction/Animal.cs
+++ Game/Assets/Scripts/Interaction/Animal.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Animal : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
-
+    [SerializeField] GameObject baseVFX;
+    [SerializeField] GameObject trailVFX;
     public string InteractionPrompt => _prompt;
     
     public bool Interact(Interactor interactor)
     {
         Debug.Log("touchy");
-        return false;
+        Destroy(gameObject);
+        Vector3 r = Vector3.zero;
+        r.x = -90;
+        Vector3 pos = transform.position;
+        pos.y += 1;
+        GameObject disapear = Instantiate(baseVFX, pos, Quaternion.Euler(r));
+        GameObject disapear2 = Instantiate(trailVFX, pos, Quaternion.Euler(r));
+        return true;
     }
 }
